ac1a01a5e66d02acb371403904f9ed1a5929088f
erenbayrakli <erenbayrakli1@gmail.com>
2023-10-14 15:31:48 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.scss';
+import 'boxicons/css/boxicons.min.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import AppLayout from './components/layout/AppLayout';
+import Blank from './pages/Blank';
+import Home from './pages/Home';
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<AppLayout />}>
+                    <Route index element={<Home />} />
+                    <Route path='/started' element={<Blank />} />
+                    <Route path='/calendar' element={<Blank />} />
+                    <Route path='/user' element={<Blank />} />
+                    <Route path='/order' element={<Blank />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+const Home = () => {
+    return <div className="content-wrapper">
+    <div className="content">
+      <div title="Home" className="lg:hidden" />
+      <p3>
+        {`Hi everyone👋 I'm Eren, a software engineer based in Istanbul.`}
+      </p3>
+      <p>
+      <br />
+      I'm Eren Bayrakli, a software developer with three years of experience.<br />
+      I'm passionate about technology, sports, music, and minimalistic design.<br />
+      On this blog, you'll find my musings, notes on professional growth, and snapshots of moments that bring me joy.
+      </p> 
+    </div>
+  </div>;
+};
+
+export default Home;
